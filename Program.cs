99ea1e2742d63a5db2688a4e606eee6b30208440
patricio <patojeder@gmail.com>
2024-05-14 16:29:33 -0300

    punto 1 resuelto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+
+int aux, numero;
+Console.Write("Ingrese un numero: ");
+string num = Console.ReadLine();
+int.TryParse(num, out numero);
+aux = numero;
+if (numero>0)
+{
+    while (aux != 0)
+    {
+        numero= aux % 10;
+        Console.Write(+numero);
+        aux=aux/10;
+    }
+}
