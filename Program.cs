f79fd43571268a184e2875918b8183ce841d778e
Patricio Javier Jeder <patojeder@gmail.com>
2024-05-26 17:59:24 -0300

    resuelto punto 2 calculadora v1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*Console.WriteLine("Hello, World!");
 int a;
 int b;
 a=10;
@@ -20,4 +20,69 @@ if (numero>0)
         Console.Write(+numero);
         aux=aux/10;
     }
+}*/
+
+
+int opcion, seguir;
+float resultado, n1, n2;
+do{
+   Console.WriteLine("Ingrese la operacion a ralizar: \n1)-Sumar\n2)-Restar\n3)-Multiplicar\n4)-Dividir\n0)-Salir ");
+   string opc= Console.ReadLine();
+   int.TryParse(opc, out opcion);
+switch (opcion)
+{
+    case 1:
+        Console.WriteLine("\ningrese el numero a\n");
+        string N1= Console.ReadLine();
+        float.TryParse(N1, out n1);
+        Console.WriteLine("\ningrese el numero b\n");
+        string N2= Console.ReadLine();
+        float.TryParse(N2, out n2);
+        resultado=n1+n2;
+        Console.WriteLine("\nEl resultado de la suma entre a y b es: "+ resultado);
+
+    break;
+
+    case 2:
+        Console.WriteLine("\ningrese el numero a\n");
+        N1= Console.ReadLine();
+        float.TryParse(N1, out n1);
+        Console.WriteLine("\ningrese el numero b\n");
+        N2= Console.ReadLine();
+        float.TryParse(N2, out n2);
+        resultado=n1-n2;
+        Console.WriteLine("\nEl resultado de la resta entre a y b es: "+ resultado);
+
+    break;
+
+    case 3:
+        Console.WriteLine("\ningrese el numero a\n");
+        N1= Console.ReadLine();
+        float.TryParse(N1, out n1);
+        Console.WriteLine("\ningrese el numero b\n");
+        N2= Console.ReadLine();
+        float.TryParse(N2, out n2);
+        resultado=n1*n2;
+        Console.WriteLine("\nEl resultado del producto a y b es: "+ resultado);
+    break;
+
+    case 4: 
+        Console.WriteLine("\ningrese el numero a\n");
+        N1= Console.ReadLine();
+        float.TryParse(N1, out n1);
+        Console.WriteLine("\ningrese el numero b\n");
+        N2= Console.ReadLine();
+        float.TryParse(N2, out n2);
+        resultado=n1/n2;
+        Console.WriteLine("\nEl resultado de la division entre a y b es: "+ resultado);
+    break;
+
+    default:
+    break;
 }
+
+        Console.WriteLine("\ndesea realizar otrocalculo?: 1-)SI\n0) NO\n ");
+        string seg= Console.ReadLine();
+        int.TryParse(seg, out seguir);
+}while(seguir != 0);
+
